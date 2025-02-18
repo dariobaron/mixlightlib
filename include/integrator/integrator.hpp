81de70adcb606c76c6d12427ccfd2bf71a6f0884
Dario Barone <barone.dario97@gmail.com>
2025-02-18 19:51:23 +0100

    added integration on unbounded domains and optimizations

--- include/integrator/integrator.hpp
+++ include/integrator/integrator.hpp
@@ -17,13 +17,14 @@ private:
 	const Func & f_;
 	double a_, b_;
 	double tolerance_;
+	double scale_, loc_;
 	double result_;
 	double error_;
 	unsigned maxiter_;
 	unsigned nSubintervals_;
 	bool success_;
 public:
-	Integrator(const Func & f, double a, double b, double tolerance, unsigned maxiter=1000);
+	Integrator(const Func & f, double a, double b, double tolerance, double scale=1, double loc=0, unsigned maxiter=100);
 	double a() const;
 	double b() const;
 	double tolerance() const;
@@ -32,22 +33,32 @@ public:
 	unsigned nSubintervals() const;
 	bool success() const;
 private:
+	bool reverse_;
 	void integrate();
 };
 
 
 template <typename Func, typename T>
-Integrator<Func,T>::Integrator(const Func & f, double a, double b, double tolerance, unsigned maxiter)
-												: f_(f), a_(std::min(a,b)), b_(std::max(a,b)), tolerance_(tolerance),
-												result_(0), error_(0), maxiter_(maxiter), nSubintervals_(0), success_(false)
+Integrator<Func,T>::Integrator(const Func & f, double a, double b, double tolerance, double scale, double loc, unsigned maxiter)
+												: f_(f), a_(std::min(a,b)), b_(std::max(a,b)), tolerance_(tolerance), scale_(scale), loc_(loc),
+												result_(0), error_(0), maxiter_(maxiter), nSubintervals_(0), success_(false), reverse_(a>b)
 {
 	integrate();
+	if (reverse_){
+		result_ = -result_;
+	}
+}
+
+
+template <typename Func, typename T>
+double Integrator<Func,T>::a() const{
+	return reverse_ ? b_ : a_;
 }
 
 
 template <typename Func, typename T>
 double Integrator<Func,T>::b() const{
-	return b_;
+	return reverse_ ? a_ : b;
 }
 
 
@@ -84,27 +95,20 @@ bool Integrator<Func,T>::success() const{
 template <typename Func, typename T>
 void Integrator<Func,T>::integrate(){
 	std::list<Solver> subintegrals;
-	{
-		FuncWrapper mapper(f_, a_, b_);
-		subintegrals.emplace_back(mapper, tolerance_);
-	}
+	subintegrals.emplace_back(FuncWrapper(f_, a_, b_, scale_, loc_), tolerance_);
 	auto current = subintegrals.begin();
 	while (current != subintegrals.end()){
 		current->integrate();
-		if (current->success()){
+		if (current->success() && subintegrals.size() > 1){
 			++current;
 		}
 		else{
 			auto to_remove = current;
 			double a = current->a();
 			double b = current->b();
-			FuncWrapper mapper(f_, a, b);
-			double a_t = mapper.transformVariable(a);
-			double b_t = mapper.transformVariable(b);
-			double m_t = 0.5 * (a_t + b_t);
-			double m = mapper.inverseTransformVariable(m_t);
+			double m = FuncWrapper(f_, a, b, scale_, loc_).transformVariable(0);
 			double new_tolerance = 0.5 * current->tolerance();
-			current = subintegrals.insert(current, {Solver(FuncWrapper(f_, a, m), new_tolerance), Solver(FuncWrapper(f_, m, b), new_tolerance)});
+			current = subintegrals.insert(current, {Solver(FuncWrapper(f_, a, m, scale_, loc_), new_tolerance), Solver(FuncWrapper(f_, m, b, scale_, loc_), new_tolerance)});
 			subintegrals.erase(to_remove);
 		}
 		if (subintegrals.size() > maxiter_){
@@ -121,10 +125,4 @@ void Integrator<Func,T>::integrate(){
 	error_ = std::accumulate(subintegrals.begin(), subintegrals.end(), 0.0, [](double sum, const Solver & integral){return sum + integral.error();});
 	nSubintervals_ = subintegrals.size();
 	success_ = std::all_of(subintegrals.begin(), subintegrals.end(), [](const Solver & integral){return integral.success();});
-}
-
-
-template <typename Func, typename T>
-double Integrator<Func,T>::a() const{
-	return a_;
 }
--- include/integrator/mapper.hpp
+++ include/integrator/mapper.hpp
@@ -2,7 +2,45 @@
 
 #include <limits>
 #include <stdexcept>
+#include <cmath>
 
+////////////////////////
+// This class allows to use the GaussKronrod class
+// that integrates the normalized variable t in (-1,1)
+// in a general integral of the variable x over (a,b)
+// as in $\int_{ginv(a)}^{ginv(b)} f(g(t)) g'(t) dt = \int_a^b f(x) dx$.
+// The map x = g(t) is called the transformation and t = g^-1(x) is its inverse.
+//
+// Depeneding on the finiteness of a, b, the following transformations are used:
+// - a finite, b finite:
+//			x = t * (b-a)/2 + (a+b)/2
+//			t = (2x-b-a) / (b-a)
+//			dx/dt = (b-a)/2
+// - a infinite, b finite:
+//			x = b - scale * (1-t) / (1+t)
+//			t = - (x-b+scale) / (x-b-scale)
+//			dx/dt = 2*scale / (1+t)^2
+// - a finite, b infinite:
+//			x = a + scale * (1+t) / (1-t)
+//			t = (x-a-scale) / (x-a+scale)
+//			dx/dt = 2*scale / (1-t)^2
+// - a infinite, b infinite:
+//			x = loc + 3/4 * scale * t / (1-t^2)
+//			t = (-3/4*scale + \sqrt{9/16*scale^2 + 4*(x-loc)^2}) / (2*(x-loc))
+//			dx/dt = 3/4 * scale * (1+t^2) / (1-t^2)^2
+//
+// The parameter `scale` is only considered if the interval is not bounded,
+// the parameter `loc` is only considered if the interval is unbounded.
+// In case of half-bound interval, the parameter `scale` represents the distance
+// from the finite bound at which the interval is splitted at the next iteration. It
+// must be chosen such that the splitting happens within a relevant region of
+// evaluation of the function.
+// In case of unbounded interval, the parameter `loc` represents the location
+// at which the interval is splitted at the next iteration. It must be chosen
+// at the centre of the relevant region of evaluation of the function.
+// In case of unbounded interval, the parameter `scale` represents the amplitude
+// of the relevant region of evaluation of the function.
+////////////////////////
 
 template <typename Func, typename T=double>
 class Mapper{
@@ -16,20 +54,21 @@ public:
 private:
 	const Func & f_;
 	double a_, b_;
+	double scale_, loc_;
 	Bounds bounds_;
 public:
-	Mapper(const Func & f, double a, double b);
+	Mapper(const Func & f, double a, double b, double scale=1, double loc=0);
 	double a() const;
 	double b() const;
 	double operator()(double t) const;
+	double inverseTransformVariable(double x) const;
+	double transformVariable(double t) const;
 	double jacobian(double t) const;
-	double transformVariable(double x) const;
-	double inverseTransformVariable(double t) const;
 };
 
 
 template <typename Func, typename T>
-Mapper<Func,T>::Mapper(const Func & f, double a, double b) : f_(f), a_(a), b_(b){
+Mapper<Func,T>::Mapper(const Func & f, double a, double b, double scale, double loc) : f_(f), a_(a), b_(b), scale_(scale), loc_(loc) {
 	bool is_a_inf = a_ == -std::numeric_limits<double>::infinity();
 	bool is_b_inf = b_ == std::numeric_limits<double>::infinity();
 	bounds_ = static_cast<Bounds>(2 * is_a_inf + is_b_inf);
@@ -50,10 +89,63 @@ double Mapper<Func,T>::b() const{
 
 template <typename Func, typename T>
 double Mapper<Func,T>::operator()(double t) const{
-	return jacobian(t) * f_(inverseTransformVariable(t));
+	return jacobian(t) * f_(transformVariable(t));
 }
 
 
+template <typename Func, typename T>
+double Mapper<Func,T>::inverseTransformVariable(double x) const{
+	if (x == a_){
+		return -1;
+	} else if (x == b_){
+		return 1;
+	}
+	double t;
+	switch (bounds_){
+	case Bounds::bounded:
+		t = (2*x - b_ - a_) / (b_ - a_);
+		break;
+	case Bounds::leftbounded:
+		t = (x - a_ - scale_) / (x - a_ + scale_);
+		break;
+	case Bounds::rightbounded:
+		t = - (x - b_ + scale_) / (x - b_ - scale_);
+		break;
+	case Bounds::unbounded:
+		t = (-3./4.*scale_ + std::sqrt(std::pow(3./4.*scale_, 2) + 4*std::pow(x-loc_, 2))) / (2 * (x-loc_));
+		break;
+	default:
+		throw std::runtime_error("Mapper::inverseTransformVariable: unknown bounds");
+		break;
+	}
+	return t;
+}
+
+
+template <typename Func, typename T>
+double Mapper<Func,T>::transformVariable(double t) const{
+	double x;
+	switch (bounds_){
+	case Bounds::bounded:
+		x = t * 0.5 * (b_ - a_) + 0.5 * (a_ + b_);
+		break;
+	case Bounds::leftbounded:
+		x = a_ + scale_ * (1+t) / (1-t);
+		break;
+	case Bounds::rightbounded:
+		x = b_ - scale_ * (1-t) / (1+t);
+		break;
+	case Bounds::unbounded:
+		x = loc_ + 3./4. * scale_ * t / (1-t*t);
+		break;
+	default:
+		throw std::runtime_error("Mapper::transformVariable: unknown bounds");
+		break;
+	}
+	return x;
+}
+
+	
 template <typename Func, typename T>
 double Mapper<Func,T>::jacobian(double t) const{
 	double j;
@@ -62,56 +154,17 @@ double Mapper<Func,T>::jacobian(double t) const{
 		j = 0.5 * (b_ - a_);
 		break;
 	case Bounds::leftbounded:
-		throw std::runtime_error("Mapper::jacobian: leftbounded not implemented");
+		j = 2 * scale_ / std::pow(1-t, 2);
 		break;
 	case Bounds::rightbounded:
-		throw std::runtime_error("Mapper::jacobian: rightbounded not implemented");
+		j = 2 * scale_ / std::pow(1+t, 2);
 		break;
 	case Bounds::unbounded:
-		throw std::runtime_error("Mapper::jacobian: unbounded not implemented");
+		j = 3./4. * scale_ * (1+t*t) / std::pow(1-t*t, 2);
+		break;
+	default:
+		throw std::runtime_error("Mapper::jacobian: unknown bounds");
 		break;
 	}
 	return j;
-}
-
-
-template <typename Func, typename T>
-double Mapper<Func,T>::transformVariable(double x) const{
-	double t;
-	switch (bounds_){
-	case Bounds::bounded:
-		t = (2*x - a_ - b_) / (b_ - a_);
-		break;
-	case Bounds::leftbounded:
-		throw std::runtime_error("Mapper::jacobian: leftbounded not implemented");
-		break;
-	case Bounds::rightbounded:
-		throw std::runtime_error("Mapper::jacobian: rightbounded not implemented");
-		break;
-	case Bounds::unbounded:
-		throw std::runtime_error("Mapper::jacobian: unbounded not implemented");
-		break;
-	}
-	return t;
-}
-
-
-template <typename Func, typename T>
-double Mapper<Func,T>::inverseTransformVariable(double t) const{
-	double x;
-	switch (bounds_){
-	case Bounds::bounded:
-		x = t * 0.5 * (b_ - a_) + 0.5 * (a_ + b_);
-		break;
-	case Bounds::leftbounded:
-		throw std::runtime_error("Mapper::jacobian: leftbounded not implemented");
-		break;
-	case Bounds::rightbounded:
-		throw std::runtime_error("Mapper::jacobian: rightbounded not implemented");
-		break;
-	case Bounds::unbounded:
-		throw std::runtime_error("Mapper::jacobian: unbounded not implemented");
-		break;
-	}
-	return x;
 }
